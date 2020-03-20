e5a75d864a0f4facb0b57729a77a6f626caba306
islongwei <islongwei@163.com>
2020-03-20 15:04:39 +0800

    Revert "Revert "first commit""
    This reverts commit b1f7aea4758c7c4e4d75d6675cdb2ee706e9859b.

---
+++ eladmin-common/src/main/java/me/zhengjie/config/ElPermissionConfig.java
@@ -0,0 +1,24 @@
+package me.zhengjie.config;
+
+import me.zhengjie.utils.SecurityUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Description 由于每个接口都需要给超级管理员放行,加入了自定义权限验证方式，在验证的时候默认给拥有admin权限的用户放行
+ * @Author longwei
+ * @Date 2020/3/19 16:17
+ */
+@Service(value = "el")
+public class ElPermissionConfig {
+
+    public Boolean check(String ...permissions){
+        // 获取当前用户的所有权限
+        List<String> elPermissions = SecurityUtils.getUserDetails().getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        // 判断当前用户的所有权限是否包含接口上定义的权限
+        return elPermissions.contains("admin") || Arrays.stream(permissions).anyMatch(elPermissions::contains);
+    }
+}
